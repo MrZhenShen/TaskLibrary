f2a9e1f0366640b87523b091e4c9a6710918ca81
yevhen.rozhylo <yevhen.rozhylo@sigma.sofware>
2024-03-22 17:56:45 +0200

    init environment variables

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "your-apiKey",
+    authDomain: "your-authDomain",
+    projectId: "your-projectId",
+    storageBucket: "your-storageBucket",
+    messagingSenderId: "your-messagingSenderId",
+    appId: "your-appId"
+  }
+};
